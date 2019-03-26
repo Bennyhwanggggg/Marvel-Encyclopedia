1a4b01dc5e8c3e3b7c06b946806164376546bff6
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-03-26 15:08:12 +1100

    fix md5 hash

--- backend/routes/comics.js
+++ backend/routes/comics.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const https = require("https");
 var bodyParser = require("body-parser");
 var request = require('request');
-var digest = require('digest')
+var md5 = require('md5');
 
 const url = 'https://gateway.marvel.com:443/v1/public/comics'
 const PUBLIC_KEY = `${process.env.PUBLIC_KEY}`
@@ -11,7 +11,7 @@ const PRIVATE_KEY = `${process.env.PRIVATE_KEY}`
 
 router.get('/', (req, res) => {
     const timeStamp = Date.now().toString();
-    const md5hash = digest(`${timeStamp}${PRIVATE_KEY}${PUBLIC_KEY}`)
+    const md5hash = md5(`${timeStamp}${PRIVATE_KEY}${PUBLIC_KEY}`)
     request({
         uri: url,
         qs: {
@@ -19,13 +19,9 @@ router.get('/', (req, res) => {
             apikey: PUBLIC_KEY,
             hash: md5hash
         }
-    },
-    function(error, response, body) {
-        console.log(error);
-        console.log(response);
-        console.log(body);
-    }
-)});
+    }).pipe(res);
+});
+
 
 router.get('/:id', (req, res) => {
     res.send('hi id')
