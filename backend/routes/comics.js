663eacd877306b01482f7336721f8fd145ec0cfd
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-03-26 23:26:57 +1100

    add query parameters

--- backend/routes/comics.js
+++ backend/routes/comics.js
@@ -11,10 +11,11 @@ const PRIVATE_KEY = `${process.env.PRIVATE_KEY}`
 // /comics endpoint
 router.get('/', cacheMiddleware(30), (req, res) => {
     const timeStamp = Date.now().toString();
-    const md5hash = md5(`${timeStamp}${PRIVATE_KEY}${PUBLIC_KEY}`)
+    const md5hash = md5(`${timeStamp}${PRIVATE_KEY}${PUBLIC_KEY}`);
     request({
         uri: url,
         qs: {
+            ...req.query,
             ts: timeStamp,
             apikey: PUBLIC_KEY,
             hash: md5hash
@@ -29,6 +30,7 @@ router.get('/:id', cacheMiddleware(30), (req, res) => {
     request({
         uri: `${url}/${req.params.id}`,
         qs: {
+            ...req.query,
             ts: timeStamp,
             apikey: PUBLIC_KEY,
             hash: md5hash
@@ -43,6 +45,7 @@ router.get('/:id/characters', cacheMiddleware(30), (req, res) => {
     request({
         uri: `${url}/${req.params.id}/characters`,
         qs: {
+            ...req.query,
             ts: timeStamp,
             apikey: PUBLIC_KEY,
             hash: md5hash
@@ -57,6 +60,7 @@ router.get('/:id/creators', cacheMiddleware(30), (req, res) => {
     request({
         uri: `${url}/${req.params.id}/creators`,
         qs: {
+            ...req.query,
             ts: timeStamp,
             apikey: PUBLIC_KEY,
             hash: md5hash
@@ -71,6 +75,7 @@ router.get('/:id/events', cacheMiddleware(30), (req, res) => {
     request({
         uri: `${url}/${req.params.id}/events`,
         qs: {
+            ...req.query,
             ts: timeStamp,
             apikey: PUBLIC_KEY,
             hash: md5hash
@@ -85,6 +90,7 @@ router.get('/:id/stories', cacheMiddleware(30), (req, res) => {
     request({
         uri: `${url}/${req.params.id}/stories`,
         qs: {
+            ...req.query,
             ts: timeStamp,
             apikey: PUBLIC_KEY,
             hash: md5hash
