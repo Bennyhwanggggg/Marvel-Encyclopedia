ebdbf39323bf5458438b7ca8ae112719d5479346
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-03-26 15:18:24 +1100

    finsih template end points

--- backend/routes/comics.js
+++ backend/routes/comics.js
@@ -9,6 +9,7 @@ const url = 'https://gateway.marvel.com:443/v1/public/comics'
 const PUBLIC_KEY = `${process.env.PUBLIC_KEY}`
 const PRIVATE_KEY = `${process.env.PRIVATE_KEY}`
 
+// /comics endpoint
 router.get('/', (req, res) => {
     const timeStamp = Date.now().toString();
     const md5hash = md5(`${timeStamp}${PRIVATE_KEY}${PUBLIC_KEY}`)
@@ -22,9 +23,74 @@ router.get('/', (req, res) => {
     }).pipe(res);
 });
 
-
+// /comics/{comicId} endpoint
 router.get('/:id', (req, res) => {
-    res.send('hi id')
+    const timeStamp = Date.now().toString();
+    const md5hash = md5(`${timeStamp}${PRIVATE_KEY}${PUBLIC_KEY}`);
+    request({
+        uri: `${url}/${req.params.id}`,
+        qs: {
+            ts: timeStamp,
+            apikey: PUBLIC_KEY,
+            hash: md5hash
+        }
+    }).pipe(res);
+});
+
+// /comics/{comicId}/characters endpoint
+router.get('/:id/characters', (req, res) => {
+    const timeStamp = Date.now().toString();
+    const md5hash = md5(`${timeStamp}${PRIVATE_KEY}${PUBLIC_KEY}`);
+    request({
+        uri: `${url}/${req.params.id}/characters`,
+        qs: {
+            ts: timeStamp,
+            apikey: PUBLIC_KEY,
+            hash: md5hash
+        }
+    }).pipe(res);
+});
+
+// /comics/{comicId}/creaters endpoint
+router.get('/:id/creators', (req, res) => {
+    const timeStamp = Date.now().toString();
+    const md5hash = md5(`${timeStamp}${PRIVATE_KEY}${PUBLIC_KEY}`);
+    request({
+        uri: `${url}/${req.params.id}/creators`,
+        qs: {
+            ts: timeStamp,
+            apikey: PUBLIC_KEY,
+            hash: md5hash
+        }
+    }).pipe(res);
+});
+
+// /comics/{comicId}/events endpoint
+router.get('/:id/events', (req, res) => {
+    const timeStamp = Date.now().toString();
+    const md5hash = md5(`${timeStamp}${PRIVATE_KEY}${PUBLIC_KEY}`);
+    request({
+        uri: `${url}/${req.params.id}/events`,
+        qs: {
+            ts: timeStamp,
+            apikey: PUBLIC_KEY,
+            hash: md5hash
+        }
+    }).pipe(res);
+});
+
+// /comics/{comicId}/stories endpoint
+router.get('/:id/stories', (req, res) => {
+    const timeStamp = Date.now().toString();
+    const md5hash = md5(`${timeStamp}${PRIVATE_KEY}${PUBLIC_KEY}`);
+    request({
+        uri: `${url}/${req.params.id}/stories`,
+        qs: {
+            ts: timeStamp,
+            apikey: PUBLIC_KEY,
+            hash: md5hash
+        }
+    }).pipe(res);
 });
 
 module.exports = router;
