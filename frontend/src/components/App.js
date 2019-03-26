8aee6a153b79391e9fb2ed35d9891689d7cb0cd7
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-03-26 15:51:22 +1100

    add app

---
+++ frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="ui container">
+                hi
+            </div>
+        )
+    }
+}
+
+export default App;
